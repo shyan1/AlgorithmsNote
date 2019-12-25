cacd5ff61c1c58c6d1a8904ee9151b3eac61ecab
shyan1 <all_bright@live.com>
2019-12-25 18:22:55 +0800

    Add ArrayQueue.java sample code

---
+++ ArrayQueue.java
@@ -0,0 +1,27 @@
+public class ArrayQueue {
+    private String[] items;
+    private int n = 0;
+    private int head = 0;
+    private int tail = 0;
+
+
+    public ArrayQueue(int capacity) {
+        items = new String[capacity];
+        n = capacity;
+    }
+
+    public boolean enqueue(String item) {
+        if(tail == n) return false;
+        items[tail] = item;
+        ++tail;
+        return true;
+    }
+
+    public String dequeue() {
+        if(head == tail) return null;
+
+        String ret = items[head];
+        ++head;
+        return ret;
+    }
+}
